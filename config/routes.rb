38c02fa674bfe299871c989d4921e0c4da30d9b0
oberdan <oberdan.dev@gmail.com>
2024-02-16 11:43:48 -0400

    geat: create routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "application#index"
+  
+  get '/', to: 'application#hello'
+  get "/hello", to: "application#hello"
+  get '/teste' => 'application#hello'
 end
